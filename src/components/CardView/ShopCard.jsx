5e57084adc9ff07db0b5d0b46274c4901a6fe682
Sanshai <vbn-134@yandex.ru>
2023-10-20 16:53:33 +0300

    коррекция стилей

--- src/components/CardView/ShopCard.jsx
+++ src/components/CardView/ShopCard.jsx
@@ -6,6 +6,10 @@ export const ShopItem = ({ item }) => {
             <h2 className="title">{item.name}</h2>
             <div className="color">{item.color}</div>
             <img src={item.img} alt="Какая-то картинка" className="item-img" />
+            <div className="littie-box">
+                <div className="price dollar">${item.price}</div>
+                <button className="btn card">Add to card</button>
+            </div>
         </div>
     )
 };
